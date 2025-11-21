6b24c90bc74c7b7a457798202265f28228908db7
TAN-MAC <1240973173@qq.com>
2025-11-21 22:22:56 +0800

    example code

---
+++ test/sampleFrame.py
@@ -0,0 +1,27 @@
+# from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+# class MyWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+
+
+# if __name__ == "__main__":
+#     app = QApplication([])
+#     window = MyWindow()
+#     window.show()
+#     app.exec()
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MyWindow()
+    window.show()
+    app.exec()
